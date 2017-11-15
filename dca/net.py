08499e50551a46e31303c60d5121fc50a69bf334
torstein <t.soernes@gmail.com>
2017-11-15 23:14:11 +0100

    move data loder to its own file

--- dca/net.py
+++ dca/net.py
@@ -1,12 +1,11 @@
 import sys
 
-import h5py
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
 from tensorflow.python.client import timeline
 
-from utils import BackgroundGenerator
+import dataloader
 
 
 """
@@ -100,7 +99,7 @@ class Net:
     def load_data(self):
         if self.data_is_loaded:
             return
-        data = self.get_data_h5py()
+        data = dataloader.get_data_h5py()
         # data = self.get_data()
         self.n_train_steps = data['n_train_steps']
         self.n_test_steps = data['n_test_steps']
@@ -224,75 +223,6 @@ class Net:
         self.train_writer = tf.summary.FileWriter(self.log_path + '/train',
                                                   self.sess.graph)
         self.eval_writer = tf.summary.FileWriter(self.log_path + '/eval')
-
-    def get_data_h5py(self):
-        # Open file handle, but don't load contents into memory
-        h5f = h5py.File("data-experience.0.hdf5", "r")
-        entries = len(h5f['grids'])
-
-        split_perc = 0.9  # Percentage of data to train on
-        split = int(entries * split_perc) // self.batch_size
-        end = entries // self.batch_size
-
-        def data_gen(start, stop):
-            for i in range(start, stop):
-                batch = slice(i * self.batch_size, (i + 1) * self.batch_size)
-                # Load batch data into memory and prep it
-                grids, cells, actions, rewards, next_grids, next_cells = \
-                    self.prep_data(
-                        h5f['grids'][batch][:],
-                        h5f['cells'][batch][:],
-                        h5f['chs'][batch][:],
-                        h5f['rewards'][batch][:],
-                        h5f['next_grids'][batch][:],
-                        h5f['next_cells'][batch][:])
-                yield {
-                    'grids': grids,
-                    'cells': cells,
-                    'actions': actions,
-                    'rewards': rewards,
-                    'next_grids': next_grids,
-                    'next_cells': next_cells
-                }
-
-        train_gen = BackgroundGenerator(data_gen(0, split), k=10)
-        test_gen = BackgroundGenerator(data_gen(split, end), k=10)
-        return {
-            "n_train_steps": split,
-            "n_test_steps": end - split,
-            "train_gen": train_gen,
-            "test_gen": test_gen
-        }
-
-    def get_data(self):
-        data = np.load("data-experience-shuffle-sub.npy")
-        grids, oh_cells, actions, rewards, next_grids, next_oh_cells = \
-            self.prep_data(*map(np.array, zip(*data)))
-
-        split_perc = 0.9  # Percentage of data to train on
-        split = int(len(grids) * split_perc) // self.batch_size
-        end = len(grids) // self.batch_size
-
-        def data_gen(start, stop):
-            for i in range(start, stop):
-                batch = slice(i * self.batch_size, (i + 1) * self.batch_size)
-                yield {
-                    'grids': grids[batch],
-                    'cells': oh_cells[batch],
-                    'actions': actions[batch],
-                    'rewards': rewards[batch],
-                    'next_grids': next_grids[batch],
-                    'next_cells': next_oh_cells[batch]
-                }
-
-        train_gen = data_gen(0, split)
-        test_gen = data_gen(split, end)
-        return {
-            "n_train_steps": split,
-            "n_test_steps": end - split,
-            "train_gen": train_gen,
-            "test_gen": test_gen
-        }
 
     @staticmethod
     def prep_data_grids(grids):
