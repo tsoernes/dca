bf317f88f85b864565173422c81559dd22b799fc
torstein <t.soernes@gmail.com>
2018-01-13 15:39:37 +0100

    remove some unused files; improve some filenames; add ACKTR baseline

--- dca/div/tf-lazy-cond.py
+++ dca/div/tf-lazy-cond.py
@@ -11,13 +11,11 @@ fn2 = lambda: fn()
 res = tf.cond(
     tf.equal(tf.size(a), tf.constant(0)),
     lambda: tf.constant(-1, dtype=tf.int64),
-    fn2
-    )
+    fn2)
 
 res2 = tf.cond(
     tf.equal(tf.size(a), tf.constant(0)),
     lambda: tf.constant(-1, dtype=tf.int64),
-    lambda: tf.add(fn(), tf.constant(1, dtype=tf.int64))
-    )
+    lambda: tf.add(fn(), tf.constant(1, dtype=tf.int64)))
 
 print(sess.run(res, {b: np.zeros(0, dtype=np.int64)}))
