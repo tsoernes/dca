Singh: implement scaling, testing with include_self True; implement correct dt_rewards, test results

dca/environment.py
@@ -102,8 +102,8 @@ class Env:
 
         self.cevent = self.eventgen.pop()
         dt = self.cevent[0] - t
-        disc = np.exp(-self.beta * dt)
-        return (self.reward(dt), disc, self.cevent)
+        beta_disc = np.exp(-self.beta * dt)
+        return (self.reward(beta_disc), beta_disc, self.cevent)
 
     def execute_action(self, cevent, ch: int):
         """
@@ -141,7 +141,7 @@ class Env:
                 self.logger.debug(f"Ended call cell in {cell} on ch {ch}")
             self.grid.state[cell][ch] = 0
 
-    def reward(self, dt):
+    def reward(self, beta_disc):
         """
         Immediate reward, which is the total number of calls
         currently in progress system-wide
@@ -149,5 +149,4 @@ class Env:
         count = np.count_nonzero(self.grid.state)
         if not self.dt_rewards:
             return count
-        b = self.beta
-        return ((1 - np.exp(-b * dt)) / b) * count
+        return ((1 - beta_disc) / self.beta) * count

dca/strats/base.py
@@ -49,8 +49,8 @@ class Strat:
         while self.continue_sim(i, t):
             t, ce_type, cell = cevent[0:3]
             grid = np.copy(self.grid)  # Copy before state is modified
-            reward, disc, next_cevent = self.env.step(ch)
-            next_ch = self.get_action(next_cevent, grid, cell, ch, reward, ce_type, disc)
+            reward, bdisc, next_cevent = self.env.step(ch)
+            next_ch = self.get_action(next_cevent, grid, cell, ch, reward, ce_type, bdisc)
             # NOTE Could do per-strat saving here, as they save different stuff
             if (self.save or self.batch_size > 1) \
                     and ch is not None \

dca/strats/net_rl.py
@@ -297,7 +297,7 @@ class SinghNetStrat(VNetStrat):
         else:
             chs = np.nonzero(self.grid[cell])[0]
 
-        fgrids = self.afterstate_freps(self.grid, cell, ce_type, chs)
+        fgrids = self.scale_freps(self.afterstate_freps(self.grid, cell, ce_type, chs))
         # fgrids2 = self.afterstate_freps2(self.grid, cell, ce_type, chs)
         # assert (fgrids == fgrids2).all()
         qvals_dense = self.net.forward(fgrids)
@@ -310,13 +310,23 @@ class SinghNetStrat(VNetStrat):
 
     def backward(self, grid, cell, reward, next_grid):
         value_target = reward + self.gamma * np.array([[self.val]])
-        loss = self.net.backward([self.feature_reps(grid, cell)],
-                                 [self.feature_reps(next_grid, cell)], value_target)
+        loss = self.net.backward(
+            self.scale_freps(self.feature_reps(grid)),
+            self.scale_freps(self.feature_reps(next_grid)), value_target)
         return loss
 
     def afterstate_freps_naive(self, grid, cell, ce_type, chs):
         astates = RhombusAxialGrid.afterstates_stat(grid, cell, ce_type, chs)
         freps = self.feature_reps(astates)
+        return freps
+
+    def scale_freps(self, freps):
+        assert freps.ndim == 4
+        # TODO Try Scale freps in range [-1, 1]
+        # Scale freps in range [0, 1]
+        freps = freps.astype(np.float16)
+        freps[:, :, :, :-1] /= 43.0  # Max possible neighs within dist 4 including self
+        freps[:, :, :, -1] /= float(self.n_channels)
         return freps
 
     def afterstate_freps(self, grid, cell, ce_type, chs):
@@ -326,10 +336,10 @@ class SinghNetStrat(VNetStrat):
         n_used_neighs (frep[:-1]) does include self
         n_free_self (frep[-1]) counts ELIGIBLE chs
         """
-        fgrid = self.feature_reps(grid)
+        fgrid = self.feature_reps(grid)[0]
         r, c = cell
         neighs4 = RhombusAxialGrid.neighbors(
-            dist=4, row=r, col=c, separate=True, include_self=False)
+            dist=4, row=r, col=c, separate=True, include_self=True)
         neighs2 = RhombusAxialGrid.neighbors(dist=2, row=r, col=c, include_self=True)
         fgrids = np.repeat(np.expand_dims(fgrid, axis=0), len(chs), axis=0)
         if ce_type == CEvent.END:
@@ -356,7 +366,7 @@ class SinghNetStrat(VNetStrat):
             grid[cell][chs] = 1
         return fgrids
 
-    def feature_reps(self, grids, *args):
+    def feature_reps(self, grids):
         """
         Takes a grid or an array of grids and return the feature representation(s).
 
@@ -367,23 +377,20 @@ class SinghNetStrat(VNetStrat):
         in use by the cell itself, though that may be the better option.
         """
         assert type(grids) == np.ndarray
-        single = False  # Only one grid to create frep for
         if grids.ndim == 3:
             grids = np.expand_dims(grids, axis=0)
-            single = True
         fgrids = np.zeros(
-            (len(grids), self.rows, self.cols, self.n_channels + 1), dtype=np.int32)
+            (len(grids), self.rows, self.cols, self.n_channels + 1), dtype=np.int16)
         # fgrids[:, :, :, self.n_channels] = self.n_channels \
         #     - np.count_nonzero(grids, axis=3)
         for r in range(self.rows):
             for c in range(self.cols):
-                neighs = self.env.grid.neighbors(4, r, c, separate=True)
+                neighs = self.env.grid.neighbors(
+                    4, r, c, separate=True, include_self=True)
                 n_used = np.count_nonzero(grids[:, neighs[0], neighs[1]], axis=1)
                 fgrids[:, r, c, :-1] = n_used
                 for i in range(len(grids)):
                     n_eligible_chs = RhombusAxialGrid.get_n_eligible_chs_stat(
                         grids[i], (r, c))
                     fgrids[i, r, c, -1] = n_eligible_chs
-        if single:
-            return fgrids[0]
         return fgrids

dca/strats/table_rl.py
@@ -31,16 +31,17 @@ class QTable(RLStrat):
         else:
             return self.qvals[rep][chs]
 
-    def update_qval(self, grid, cell, ch, reward, next_cell, next_ch, *args):
+    def update_qval(self, grid, cell, ch, reward, next_cell, next_ch, next_max_ch, bdisc):
         assert type(ch) == np.int64
         assert ch is not None
         if self.pp['verify_grid']:
             assert np.sum(grid != self.grid) == 1
-        # Counting n_used of self.grid instead of grid yields significantly better
-        # performance for unknown reasons.
-        next_n_used = np.count_nonzero(self.grid[cell])
+        next_n_used = np.count_nonzero(self.grid[next_cell])
         next_qval = self.get_qvals(next_cell, next_n_used, next_ch)
-        target_q = reward + self.gamma * next_qval
+        gamma = bdisc if self.pp['dt_rewards'] else self.gamma
+        target_q = reward + gamma * next_qval
+        # Counting n_used of self.grid instead of grid yields significantly better
+        # performance for (TT-)SARSA for unknown reasons.
         n_used = np.count_nonzero(grid[cell])
         q = self.get_qvals(cell, n_used, ch)
         td_err = target_q - q
@@ -50,7 +51,7 @@ class QTable(RLStrat):
         else:
             self.el_traces[frep][ch] += 1
             self.qvals += self.alpha * td_err * self.el_traces
-            self.el_traces *= self.gamma * self.lmbda
+            self.el_traces *= gamma * self.lmbda
         if self.alpha > self.pp['min_alpha']:
             self.alpha *= self.alpha_decay
         next_frep = self.feature_rep(next_cell, next_n_used)

dca/teststrats.py
@@ -35,6 +35,7 @@ class TestSinghStrat(unittest.TestCase):
         def check_freps(freps_inc, freps):
             self.assertTrue(freps_inc.shape == freps.shape,
                             (freps_inc.shape, freps.shape))
+            self.assertTrue(freps.shape[1:] == (7, 7, 71))
             eq_n_free = freps_inc[:, :, :, :-1] == freps[:, :, :, :-1]
             diff_n_free = np.where(np.invert(eq_n_free))
             self.assertTrue(eq_n_free.all(),
@@ -95,9 +96,9 @@ class TestSinghStrat(unittest.TestCase):
             (self.pp['rows'], self.pp['cols'], self.pp['n_channels']), dtype=bool)
         grid2[:, :, 0] = 1
         grid3[1, 2, 9] = 1
-        fgrid1 = self.strat.feature_reps(grid1)
-        fgrid2 = self.strat.feature_reps(grid2)
-        fgrid3 = self.strat.feature_reps(grid3)
+        fgrid1 = self.strat.feature_reps(grid1)[0]
+        fgrid2 = self.strat.feature_reps(grid2)[0]
+        fgrid3 = self.strat.feature_reps(grid3)[0]
 
         # Verify that single- and multi-version works the same
         grids = np.array([grid1, grid2, grid3])
@@ -126,7 +127,7 @@ class TestSinghStrat(unittest.TestCase):
         for r in range(self.rows):
             for c in range(self.cols):
                 n_neighs = len(RhombusAxialGrid.neighbors(4, r, c))
-                n_used2[r][c][0] = n_neighs
+                n_used2[r][c][0] = n_neighs + 1
         check_n_used_neighs(fgrid2, n_used2)
 
         # Verify Grid #3
@@ -136,6 +137,6 @@ class TestSinghStrat(unittest.TestCase):
         # Cell (1, 2) has no neighs that use ch9. The neighs of (1, 2)
         # has 1 cell that use ch9.
         n_used3 = np.zeros((self.rows, self.cols, self.n_channels))
-        neighs3 = RhombusAxialGrid.neighbors(4, 1, 2, separate=True)
+        neighs3 = RhombusAxialGrid.neighbors(4, 1, 2, separate=True, include_self=True)
         n_used3[(*neighs3, np.repeat(9, len(neighs3[0])))] = 1
         check_n_used_neighs(fgrid3, n_used3)