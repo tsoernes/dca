f14452a76e086d529dda6eadce7806f91713175c
torstein <t.soernes@gmail.com>
2018-03-03 23:52:11 +0100

    add exploration policy with greedy select from nominal chs

--- dca/strats/exp_policies.py
+++ dca/strats/exp_policies.py
@@ -24,13 +24,31 @@ def policy_eps_greedy(epsilon, chs, qvals_dense, *args):
 
 
 def policy_nom_eps_greedy(epsilon, chs, qvals_dense, cell):
-    """Epsilon greedy where exploration actions are selected from
+    """Epsilon greedy where exploration actions are selected randomly from
     the cells nominal channels, if possible"""
     if np.random.random() < epsilon:
         # Choose at random, but prefer nominal channels
         nom_elig = _nominal_eligible_chs(chs, cell)
         if nom_elig:
             ch = np.random.choice(nom_elig)
+        else:
+            ch = np.random.choice(chs)
+    else:
+        # Choose greedily
+        idx = np.argmax(qvals_dense)
+        ch = chs[idx]
+    return ch
+
+
+def policy_nom_eps_greedy2(epsilon, chs, qvals_dense, cell):
+    """Epsilon greedy where exploration actions are selected greedily from
+    the cells nominal channels, if possible"""
+    if np.random.random() < epsilon:
+        # Choose at random, but prefer nominal channels
+        nom_elig = _nominal_eligible_chs(chs, cell)
+        if nom_elig:
+            idx = np.argmax(nom_elig)
+            ch = chs[idx]
         else:
             ch = np.random.choice(chs)
     else:
@@ -50,9 +68,7 @@ def policy_boltzmann(temp, chs, qvals_dense, *args):
 def policy_nom_boltzmann(temp, chs, qvals_dense, cell):
     nom_elig = _nominal_eligible_chs(chs, cell)
     if nom_elig:
-        scaled = np.exp((nom_elig - np.max(nom_elig)) / temp)
-        probs = scaled / np.sum(scaled)
-        ch = np.random.choice(nom_elig, p=probs)
+        ch = policy_boltzmann(temp, nom_elig, qvals_dense)
     else:
         ch = policy_boltzmann(temp, chs, qvals_dense)
     return ch
@@ -61,6 +77,7 @@ def policy_nom_boltzmann(temp, chs, qvals_dense, cell):
 exp_pol_funcs = {
     'eps_greedy': policy_eps_greedy,
     'nom_eps_greedy': policy_nom_eps_greedy,
+    'nom_eps_greedy2': policy_nom_eps_greedy2,
     'boltzmann': policy_boltzmann,
     'nom_boltzmann': policy_nom_boltzmann
 }  # yapf: disable
