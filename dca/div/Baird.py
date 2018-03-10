remove duplicate LSTD file; speed up LSTD and fix value calculation; allow dt_rewards without beta discount

dca/div/Baird.py
@@ -178,10 +178,8 @@ def expectedEmphaticTD(theta, emphasis, alpha):
         nextState = LOWER_STATE
         delta = REWARD + DISCOUNT * np.dot(FEATURES[nextState, :], theta) - np.dot(
             FEATURES[state, :], theta)
-        expectedUpdate += 1.0 / len(
-            STATES
-        ) * BEHAVIOR_SOLID_PROBABILITY * nextEmphasis * 1 / BEHAVIOR_SOLID_PROBABILITY * delta * FEATURES[
-            state, :]
+        expectedUpdate += 1.0 / len(STATES) * BEHAVIOR_SOLID_PROBABILITY \
+            * nextEmphasis * 1 / BEHAVIOR_SOLID_PROBABILITY * delta * FEATURES[state, :]
     theta += alpha * expectedUpdate
     return expectedNextEmphasis / len(STATES)
 

dca/strats/vnet_rl.py
@@ -130,15 +130,14 @@ class AvgSinghNetStrat(VNetStrat):
 
 
 class LSTDSinghNetStrat(VNetStrat):
+    """Least Squares
+    Good starting point
+    --alpha 0.00000001 -epol greedy --beta 2600
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.net = LSTDSinghNet(self.pp, self.logger)
-
-    def update_qval(self, grid, cell, ce_type, ch, reward, next_grid, next_cell, next_val,
-                    discount):
-        frep, next_freps = NGF.successive_freps(grid, cell, ce_type, np.array([ch]))
-        self.net.backward(
-            frep=frep, reward=reward, next_frep=next_freps[0], gamma=discount)
 
 
 class PSinghNetStrat(VNetStrat):