71b2bd14c22219efaa3d3ad73c8aa2b2195966b0
torstein <t.soernes@gmail.com>
2018-04-11 18:16:57 +0200

    (TDC-NL) Add huber loss

--- dca/nets/singh_tdc_tf.py
+++ dca/nets/singh_tdc_tf.py
@@ -36,18 +36,24 @@ class TFTDCSinghNet(Net):
         self.value = tf.matmul(freps_rowvec, hidden)
         next_value = tf.matmul(next_freps_rowvec, hidden)
 
-        self.td_err = self.reward - self.avg_reward + self.discount * next_value - self.value
+        td_err = self.reward - self.avg_reward + self.discount * next_value - self.value
+        if self.pp['huber_loss'] is not None:
+            err = tf.abs(td_err)
+            mg = tf.constant(self.pp['huber_loss'])
+            self.loss = tf.where(err < mg, td_err, mg * tf.sign(td_err))
+        else:
+            self.loss = td_err
         dot = tf.matmul(freps_rowvec, self.weights)
         # Multiply by 2 to get equivalent magnitude to MSE
         # Multiply by -1 because SGD-variants inverts grads
-        grads = (-2 * self.td_err) * freps_colvec - (2 * self.avg_reward) + (
+        grads = (-2 * self.loss) * freps_colvec - (2 * self.avg_reward) + (
             2 * self.discount * dot) * next_freps_colvec
         grads_and_vars = [(grads, hidden)]
         trainer, self.lr, global_step = build_default_trainer(**self.pp)
         self.do_train = trainer.apply_gradients(grads_and_vars, global_step=global_step)
 
         with tf.control_dependencies([self.do_train]):
-            diff = (self.ph_grad_beta * (self.td_err - dot)) * freps_colvec
+            diff = (self.ph_grad_beta * (self.loss - dot)) * freps_colvec
             self.update_weights = self.weights.assign_add(diff)
 
         return None, None
@@ -86,7 +92,7 @@ class TFTDCSinghNet(Net):
             self.ph_grad_beta: self.grad_beta
         }
         lr, td_err, _, _ = self.sess.run(
-            [self.lr, self.td_err, self.do_train, self.update_weights],
+            [self.lr, self.loss, self.do_train, self.update_weights],
             feed_dict=data,
             options=self.options,
             run_metadata=self.run_metadata)
